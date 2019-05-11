b8e05bebf1cc5acc43ceda10a326ecae2d377d70
iuliapriscornita <iulia.priscornita@gmail.com>
2019-05-11 12:05:58 +0300

    initial commit

---
+++ Lab2-web-api/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2_web_api.Models
+{
+    public enum MovieGenre
+    {
+        Action,
+        Comedy,
+        Horror,
+        Thriller  
+    }
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set;}
+        public string Description { get; set;}
+        public string Genre { get; set;}
+        [EnumDataType(typeof(MovieGenre))]
+        public int Duration { get; set;}
+        public int YearOfRelease { get; set;}
+        public string Director { get; set;}
+        public DateTime DateAdded { get; set;}
+        public int Rating { get; set;}
+        [Range (1,10)]
+        public bool Watched { get; set;}
+       // public List<Comment> Comments { get; set; }
+    }
+}
---
+++ Lab2-web-api/Models/MoviesDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2_web_api.Models
+{
+    public class MoviesDbContext :DbContext
+    {
+
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet = Repository
+        // DbSet = O tabela din baza de date
+
+        public DbSet<Movie> Movies { get; set; }
+      //  public DbSet<Comment>Coments { get; set; }
+
+        
+        // public DbSet<Comment> Comments { get; set; }
+
+    }
+}
